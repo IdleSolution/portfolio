c3aaef1ccbaf2b635b49e487271baf4cd1d214f6
IdleSolution <idlesoluszyn@gmail.com>
2021-04-05 15:34:27 +0200

    Removed unnecessary packages

--- src/templates/projectTemplate.js
+++ src/templates/projectTemplate.js
@@ -2,31 +2,15 @@ import React, { useEffect, useState } from 'react'
 import { Container, Header, Text, Card, ImgContainer, Description, TextDescription,
      Side, Box, BoxBody, BoxHeader, Technologies, Technology, TechnologiesMobile } from './projectTemplateStyles';
 import { Footer } from './../components/common/Footer/index';
-import loadable from '@loadable/component'
 import '@splidejs/splide/dist/css/themes/splide-default.min.css';
 import { Splide, SplideSlide } from '@splidejs/react-splide'
-// const Splide = loadable.lib(() => import('@splidejs/react-splide'), {
-//     resolveComponent: (components) => components.Splide,
-// })
-// const SplideSlide = loadable.lib(() => import('@splidejs/react-splide'), {
-//     resolveComponent: (components) => components.SplideSlide,
-// })
+
 
 
 const ProjectTemplate = ({ pageContext }) => {
     const { html, online, imgs, technologies, title, description } = pageContext;
     const isWindow = typeof window !== "undefined" && window
-    // const [Splide, SetSplide] = useState(null);
-    // const [SplideSlide, SetSplideSlide] = useState(null);
 
-    // useEffect(() => {
-    //     if(!isWindow) return;
-    //     const { Splide, SplideSlide } = require('@splidejs/react-splide');
-    //     SetSplide(Splide);
-    //     SetSplideSlide(SplideSlide);
-    // }, [isWindow])
-
-    // if(!isWindow) return <div></div>
 
     const checkWindow = () => {
         return typeof window !== "undefined";
